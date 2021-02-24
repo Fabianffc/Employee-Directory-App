8533b0a78d93928265eed652a7fc49d4b3e079d5
Fabian <fabianffc@gmail.com>
2021-02-24 15:41:54 -0600

    Updates

---
+++ precache-manifest.a31ffabbf55d4d1326ab251a920969c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c547f5442e37ddf1f24c0a11452333a",
+    "url": "/Employee-Directory-App/./index.html"
+  },
+  {
+    "revision": "6f3e8bb6f957d5a7030e",
+    "url": "/Employee-Directory-App/./static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "1bd2017393ff83813513",
+    "url": "/Employee-Directory-App/./static/css/main.8c316816.chunk.css"
+  },
+  {
+    "revision": "6f3e8bb6f957d5a7030e",
+    "url": "/Employee-Directory-App/./static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "1bd2017393ff83813513",
+    "url": "/Employee-Directory-App/./static/js/main.654ee3ea.chunk.js"
+  },
+  {
+    "revision": "8a88cbf06315fe2e8fe3",
+    "url": "/Employee-Directory-App/./static/js/runtime~main.a332ae9e.js"
+  }
+]);
